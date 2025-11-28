f2d1fc8236e2b2b4447ff3c5968f736b7829ab1e
JoaoDiasMarques <joao.d.marques@ua.pt>
2025-11-28 11:27:10 +0000

    Space invaders with scores and stuff the guy with most hair did

---
+++ space.py
@@ -0,0 +1,485 @@
+#!/bin/python3
+import turtle
+import random
+import time
+import os
+import sys
+
+#from extras import *
+
+# =========================
+# Parâmetros / Constantes
+# =========================
+LARGURA, ALTURA = 700, 600
+BORDA_X = (LARGURA // 2) - 20
+BORDA_Y = (ALTURA // 2) - 10
+
+PLAYER_SPEED = 20
+PLAYER_BULLET_SPEED = 16
+
+ENEMY_ROWS = 3
+ENEMY_COLS = 10
+ENEMY_SPACING_X = 60
+ENEMY_SPACING_Y = 60
+ENEMY_SIZE = 32
+ENEMY_START_Y = BORDA_Y - ENEMY_SIZE    # topo visível
+ENEMY_START_X = -BORDA_X + ENEMY_SIZE
+ENEMY_FALL_SPEED = 0.5
+ENEMY_DRIFT_STEP = 2
+ENEMY_FIRE_PROB = 0.006
+ENEMY_BULLET_SPEED = 4
+ENEMY_INVERT_CHANCE = 0.05
+ENEMY_DRIFT_CHANCE = 0.5
+
+COLLISION_RADIUS = 10
+HIGHSCORES_FILE = "highscores.txt"
+SAVE_FILE = "savegame.txt"
+TOP_N = 10
+
+STATE = None  # usado apenas para callbacks do teclado
+
+# ==================
+# boolean functions
+# ==================
+
+def verifyOutOfBoundariesWidth(x):
+    if x <= -BORDA_X+20 or x >= BORDA_X-20:
+        return True
+    return False
+
+def verifyOutOfBoundariesHeight(y):
+    if y <= -BORDA_Y or y >= BORDA_Y:
+        return True
+    return False
+
+def determinateEventExecution(probability):
+    return random.random() < probability
+
+# =========================
+# Top Resultados (Highscores)
+# =========================
+def ler_highscores(filename):
+    highscores=[]
+    if os.path.exists(filename):
+        f = open(filename, 'r')
+        linhas = f.readlines()
+        f.close()
+        for linha in linhas:
+            linha=linha.strip("\n")
+            username, pontos = linha.split(":")
+            highscores.append((username, int(pontos)))
+    highscores.sort(key=lambda x: x[1], reverse=True) #lambda recebe o x e devolve o x[1]   maneira mais facil de ordenar os highscores através dos pontos(já não me lembro quem me tinha explicado a stor ou bernardo)
+    return highscores[:TOP_N] #vai devolver os highscores ordenados até chegar ao numero maximo. (TOP 10)
+
+def atualizar_highscores(filename, score):
+    highscores = ler_highscores(filename)
+    if len(highscores)<TOP_N or score>highscores[-1][1]:
+        username=input("É CAMELO o teu score foi "+ str(score)+" in chock :O ... \nVá escreve lá o teu nome para ficar registado")
+        highscores.append((username,score))
+        highscores.sort(key=lambda x: x[1], reverse=True)
+        highscores = highscores[:TOP_N]
+
+        f=open(filename,"w")
+        for username, score in highscores:
+            f.write(username+":"+str(score)+"\n")
+        f.close()
+        print("tá guardado chefe \n")
+
+    print("\n=== TOP HIGHSCORES ===")
+    for i in highscores:
+        print(i[0]+":"+str(i[1]))
+
+# =========================
+# Guardar / Carregar estado (texto)
+# =========================
+def guardar_estado_txt(filename, state):
+    guardar_jogo={
+        "player_pos": state["player"].pos(),
+        "enemies_pos": [enemy.pos() for enemy in state["enemies"]],
+        "enemy_moves": state["enemy_moves"],
+        "player_bullets": [b.pos() for b in state["player_bullets"]],
+        "enemy_bullets": [b.pos() for b in state["enemy_bullets"]],
+        "score": state["score"],
+        "frame": state["frame"]
+    }
+    f = open(filename, "w")
+
+    #player
+    x, y = guardar_jogo["player_pos"]
+    f.write("player:" + str(x) + "," + str(y) + "\n")
+
+    # Inimigos
+    for i, (x, y) in enumerate(guardar_jogo["enemies_pos"]):
+        if i < len(guardar_jogo["enemy_moves"]):
+            direction = guardar_jogo["enemy_moves"][i]
+        else:
+            direction = 1  # Direção padrão       
+        #direction = guardar_jogo["enemy_moves"][i] estava assim tive de alterar porque ele estava a tentar iterar uma lista vazia quando guardava
+        f.write("enemy:" + str(x) + "," + str(y) + "," + str(direction) + "\n")
+
+    # Balas do jogador
+    for x, y in guardar_jogo["player_bullets"]:
+        f.write("pbullet:" + str(x) + "," + str(y) + "\n")
+
+    # Balas dos inimigos
+    for x, y in guardar_jogo["enemy_bullets"]:
+        f.write("ebullet:" + str(x) + "," + str(y) + "\n")
+
+    # Score e frame
+    f.write("score:" + str(guardar_jogo['score']) + "\n")
+    f.write("frame:" + str(guardar_jogo['frame']) + "\n")
+
+    f.close()
+    print("Jogo guardado em " + filename)
+    return True
+
+def carregar_estado_txt(filename):
+    if not filename or not os.path.exists(filename):
+        print(f"Ficheiro {filename} não encontrado. Iniciando novo jogo.")
+        return False
+    state={
+        'score': 0,
+        'frame': 0,
+        'enemies': [],
+        'ememy_moves': [],
+        'player_bullets': [],
+        'enemy_bullets': [],
+        'player_pos': (0, -350)
+    }
+    f = open(filename, 'r')
+    for linha in f:
+        linha = linha.strip()
+        if not linha or ':' not in linha:
+            continue
+        
+        tipo, dados = linha.split(':', 1)
+        
+        if tipo == "score":
+            state['score'] = int(dados)
+        elif tipo == "frame":
+            state['frame'] = int(dados)
+        elif tipo == "player":
+            x, y = dados.split(',')
+            state['player_pos'] = (float(x), float(y))
+        elif tipo == "enemy":
+            x, y, direction = dados.split(',')
+            state['enemies'].append((float(x), float(y), int(direction)))
+        elif tipo == "pbullet":
+            x, y = dados.split(',')
+            state['player_bullets'].append((float(x), float(y)))
+        elif tipo == "ebullet":
+            x, y = dados.split(',')
+            state['enemy_bullets'].append((float(x), float(y)))
+    
+    f.close()
+    print(f"Jogo carregado de {filename}")
+    return state
+
+# =========================
+# Criação de entidades (jogador, inimigo e balas)
+# =========================
+def criar_entidade(x,y, tipo="enemy"):
+    t = turtle.Turtle(visible=True)
+    if tipo == "player":
+        t.shape("player.gif")
+    else:
+        t.shape("enemy.gif")
+    t.penup()
+    t.setposition(x,y)
+    return t 
+
+def criar_bala(x, y, tipo):
+    t = turtle.Turtle(visible=False)
+    t.penup()
+    t.setpos(x,y)
+    t.shape("square")
+    t.shapesize(0.8, 0.08, 5)
+    if tipo == "player":
+        t.color("yellow") ## Alterar para utilizar com inimigos
+    else:
+        t.color("red")
+    t.showturtle()
+    return t
+
+def spawn_inimigos_em_grelha(state, posicoes_existentes, dirs_existentes=None):
+    enemies = state["enemies"]
+    for enemy_row in range(0, ENEMY_ROWS):
+        y_enemy = ENEMY_START_Y - enemy_row*ENEMY_SPACING_Y
+        for enemy_col in range(0, ENEMY_COLS):
+            x_enemy = ENEMY_START_X + enemy_col*ENEMY_SPACING_X
+            enemy = criar_entidade(x_enemy, y_enemy)
+            enemy.penup()
+            enemies.append( enemy )
+    return
+
+def restaurar_balas(state, lista_pos, tipo):
+    return
+
+# =========================
+# Movimento de utilizador
+# =========================
+def move_player(direction):
+    player = STATE["player"]
+    x,y = player.pos()
+    new_x = x + direction*PLAYER_SPEED
+    if verifyOutOfBoundariesWidth(new_x):
+        return
+    player.setx(new_x)
+
+# =========================
+# Handlers de tecl
+# =========================
+def mover_esquerda_handler():
+    move_player(-1)
+
+def mover_direita_handler():
+    move_player(1)
+
+def disparar_handler():
+    player = STATE["player"]
+    x,y = player.pos()
+    STATE["player_bullets"].append(criar_bala(x,y+10, "player"))
+    return
+
+def gravar_handler():
+    filename=input("nome do ficheiro que vai ficar guardado men ").strip()
+    guardar_estado_txt(filename,STATE)
+    print("tá gravado cromo")
+    return
+
+def terminar_handler():
+    if STATE.get("jogo_terminado", False):
+        return #evitar chamar várias vezes o terminar o jogo, deu varios erros não fixes
+    
+    STATE["jogo_terminado"] = True
+
+    highscores = ler_highscores(HIGHSCORES_FILE)
+    if len(highscores)<TOP_N or STATE["score"]>highscores[-1][1]:
+        print("GRANDEEEE +1 para o top10")
+        username = input("Escreve o teu nome para ficar registado: ")
+        highscores.append((username, STATE["score"]))
+        highscores.sort(key=lambda x: x[1], reverse=True)
+        highscores = highscores[:TOP_N]
+
+        f = open(HIGHSCORES_FILE, "w")
+        for nome, pontos in highscores:
+            f.write(nome + ":" + str(pontos) + "\n")
+        f.close()
+        print("Highscore guardado!")
+    
+    print("\n=== TOP HIGHSCORES ===")
+    highscores_finais = ler_highscores(HIGHSCORES_FILE)
+    for i in highscores_finais:
+        print(i[0] + ":" + str(i[1]))
+
+
+    #tudo iterações falhadas
+    #atualizar_highscores(HIGHSCORES_FILE, STATE["score"])
+    #if STATE["score"] > 0:
+        #atualizar_highscores(HIGHSCORES_FILE, STATE["score"])
+    #else:
+        #print("\n=== TOP HIGHSCORES ===")
+        #highscores = ler_highscores(HIGHSCORES_FILE)
+        #for i in highscores:
+            #print(i[0]+":"+str(i[1]))
+    
+    STATE["screen"].bye()
+    sys.exit()
+
+def power_up_handler():
+    power_up = STATE["power_up"]
+    bullet = STATE["player_bullets"][len(STATE["player_bullets"]) -1] # obter última bala adicionada
+    power_up["activated"] = True
+    power_up["killing_area_center"] = bullet.pos()
+    return
+
+# =========================
+# Atualizações e colisões
+# =========================
+def atualizar_balas_player(state):
+    for bl in state["player_bullets"]:
+        x, y = bl.pos()
+        if y > BORDA_Y + 20:
+            bl.hideturtle()
+            state["player_bullets"].remove(bl)
+        bl.setpos(x, y + PLAYER_BULLET_SPEED)
+    return
+
+def atualizar_balas_inimigos(state):
+    for ebl in state["enemy_bullets"]:
+        x, y = ebl.pos()
+        if y < -BORDA_Y + 20:
+            ebl.hideturtle()
+            state["enemy_bullets"].remove(ebl)
+        ebl.setpos(x, y - ENEMY_BULLET_SPEED)
+    return
+
+def atualizar_inimigos(state):
+    may_i_drift = determinateEventExecution(ENEMY_DRIFT_CHANCE)
+    invert = state["enemy_invert"]
+    for enemy in state["enemies"]:
+        x, y = enemy.pos()
+        new_x = x #+ ENEMY_DRIFT_STEP*may_i_drift*invert
+        new_y = y #- ENEMY_FALL_SPEED
+        if verifyOutOfBoundariesWidth(new_x+10):
+            state["enemy_invert"] = -invert
+        enemy.setx(new_x)
+        enemy.sety(new_y)
+    return
+
+def inimigos_disparam(state):
+    for enemy in state["enemies"]:
+        x, y = enemy.pos()
+        if determinateEventExecution(ENEMY_FIRE_PROB):
+            state["enemy_bullets"].append(criar_bala(x,y-10, "enemy"))
+    return
+
+def verificar_colisoes_player_bullets(state):
+    for bullet in state["enemy_bullets"]:
+        bx, by = bullet.pos()
+        x, y = state["player"].pos()
+        if ((x-bx)**2 + (y-by)**2) <= COLLISION_RADIUS**2:
+            state["player"].hideturtle()
+            bullet.hideturtle()
+            state["enemy_bullets"].remove(bullet)
+            terminar_handler() #rever o porquê de isto funcionar, foi apenas uma hunch mas funcionou
+            state["game_over"] = True
+    return
+
+def verificar_colisoes_enemy_bullets(state):
+    for bullet in state["player_bullets"]:
+        bx, by = bullet.pos()
+        for enemy in state["enemies"]:
+            x, y = enemy.pos()
+            radius = COLLISION_RADIUS
+            if ((x-bx)**2 + (y - by)**2 <= radius**2):
+                enemy.hideturtle()
+                bullet.hideturtle()
+                state["enemies"].remove(enemy)
+                state["player_bullets"].remove(bullet)
+                state["score"] += 100 #apenas para testar o score
+    return
+
+def inimigo_chegou_ao_fundo(state):
+    for enemy in state["enemies"]:
+        x, y = enemy.pos()
+        if y <= -BORDA_Y + 20:
+            return True
+    return False
+
+def verificar_colisao_player_com_inimigos(state):
+    player_x, player_y = state["player"].pos()
+    for enemy in state["enemies"]:
+        enemy_x, enemy_y = enemy.pos()
+        if ((player_x - enemy_x)**2 + (player_y - enemy_y)**2) <= (COLLISION_RADIUS * 2)**2:
+            return True
+    return False
+
+# =========================
+# Execução principal
+# =========================
+if __name__ == "__main__":
+    # Pergunta inicial
+    filename = input("Carregar jogo? Se sim, escreva nome do ficheiro, se não carregue Return: ").strip()
+    loaded = carregar_estado_txt(filename)
+
+    # Ecrã
+    screen = turtle.Screen()
+    screen.title("Space Invaders IPRP")
+    screen.bgcolor("black")
+    screen.setup(width=LARGURA, height=ALTURA)
+    screen.tracer(0)
+
+    # Imagens obrigatórias
+    for img in ["player.gif", "enemy.gif"]:
+        if not os.path.exists(img):
+            print("ERRO: imagem '" + img + "' não encontrada.")
+            sys.exit(1)
+        screen.addshape(img)
+
+    # Estado base
+    state = {
+        "screen": screen,
+        "player": None,
+        "enemies": [],
+        "enemy_invert": 1,
+        "enemy_moves": [],
+        "player_bullets": [],
+        "enemy_bullets": [],
+        "game_over": False,
+        "power_up": {"killing_area_center": None, "activated": False},
+        "score": 0,
+        "frame": 0,
+        "files": {"highscores": HIGHSCORES_FILE, "save": SAVE_FILE},
+        "jogo_terminado": False  
+    }
+
+    # Construção inicial
+    if loaded:
+        print("Tá a funfar") # func(var[0], var[1]) = func(*var)         Nota: assumindo que var tem apenas duas entrada na lista
+        state["player"] = criar_entidade(*loaded['player_pos'], "player")
+        state["score"] = loaded['score']
+        state["frame"] = loaded['frame']
+
+        # Recriar inimigos com posições e direções
+        for x, y, direction in loaded['enemies']:
+            enemy = criar_entidade(x, y, "enemy")
+            state["enemies"].append(enemy)
+            state["enemy_moves"].append(direction)
+        
+        # Recriar balas
+        for x, y in loaded['player_bullets']:
+            bullet = criar_bala(x, y, "player")
+            state["player_bullets"].append(bullet)
+        
+        for x, y in loaded['enemy_bullets']:
+            bullet = criar_bala(x, y, "enemy")
+            state["enemy_bullets"].append(bullet)
+    else:
+        print("New game!")
+        state["player"] = criar_entidade(0, -200, "player") ## inicial: 0, -350
+        spawn_inimigos_em_grelha(state, None, None)
+
+    # Variavel global para os keyboard key handlers
+    STATE = state
+
+    # Teclas
+    screen.listen()
+    screen.onkeypress(mover_esquerda_handler, "a")
+    screen.onkeypress(mover_esquerda_handler, "Left")
+    screen.onkeypress(mover_direita_handler, "d")
+    screen.onkeypress(mover_direita_handler, "Right")
+    screen.onkeypress(disparar_handler, "space")
+    screen.onkeypress(gravar_handler, "g")
+    screen.onkeypress(terminar_handler, "Escape")
+    screen.onkeypress(power_up_handler, "p")
+
+    # Loop principal
+    while True:
+        atualizar_balas_player(STATE)
+        atualizar_inimigos(STATE)
+        inimigos_disparam(STATE)
+        atualizar_balas_inimigos(STATE)
+        verificar_colisoes_player_bullets(STATE)
+        verificar_colisoes_enemy_bullets(STATE)
+        
+        if verificar_colisao_player_com_inimigos(STATE):
+            print("Colisão direta com inimigo! Game Over")
+            terminar_handler()
+        
+        if STATE["game_over"]:
+            print("Atingido por inimigo! Game Over")
+            terminar_handler()
+
+        if inimigo_chegou_ao_fundo(STATE):
+            print("Um inimigo chegou ao fundo! Game Over")
+            terminar_handler()
+
+        if len(STATE["enemies"]) == 0:
+             print("Vitória! Todos os inimigos foram destruídos.")
+             STATE["score"] += 500 #apenas para testar a porcaria do score
+             terminar_handler()
+
+        STATE["frame"] += 1
+        screen.update()
+        time.sleep(0.016)
